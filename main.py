f285f7021119770fe742a68c053c1a4eceffcdcd
xenia-melman <xenia.melman@gmail.com>
2022-05-13 18:15:00 +0200

    added confidence score extraction

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-
-
-# def dataset_caller():
-#   class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-#  index = 23456
-# dataset_1 = DatasetHandler(datasets.cifar10, class_names)
-# dataset_1.print(index)
-
-
-# dataset_caller()
+# main incoming soon
