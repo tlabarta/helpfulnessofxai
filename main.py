41ea3a1cdd030b43866319e7fb055bafbb064641
xenia-melman <xenia.melman@gmail.com>
2022-05-16 16:59:58 +0200

    vgg16, alexnet, gradcam separated implementation that can be further used for other xai methods

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# main incoming soon
+import alexnet_model
+from vgg_model import predict
+import data_handler
+import gradcam
+
+# define model
+model = alexnet_model.train()
+
+# import image
+img_folder = './data/'
+img = data_handler.get_image(img_folder)
+
+# let model do a prediction
+predictions = predict(model, img)
+
+# get all class labels
+labels = data_handler.get_labels()
+
+# use xai to explain model prediction
+imgpath = './data/images/gazelle.jpg'
+gradcam.explain(model, imgpath)
