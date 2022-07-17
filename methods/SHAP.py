1dfb534e68ad512d25d9394e23a7fd99caa77eb1
toblab96 <labarta@tu-berlin.de>
2022-07-17 16:32:49 +0000

    added shap code source

--- methods/SHAP.py
+++ methods/SHAP.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import shap
 
+#Source: https://shap-lrjball.readthedocs.io/en/latest/example_notebooks/gradient_explainer/Explain%20an%20Intermediate%20Layer%20of%20VGG16%20on%20ImageNet.html
+
 
 def explain(model, img, org_img, labels):
     img = deepcopy(img)
@@ -15,13 +17,11 @@ def explain(model, img, org_img, labels):
     labels = np.vectorize(lambda x: labels[str(x)][1])(indexes)
     shap_values = [np.swapaxes(np.swapaxes(s, 2, 3), 1, -1) for s in shap_values]
 
-    ##############Move to Shap folder#######################
     from matplotlib.colors import LinearSegmentedColormap
     cmap = plt.cm.bwr
     bwr = cmap(np.arange(cmap.N))
     bwr[:, -1] = np.linspace(0, 1, cmap.N)
     bwr = LinearSegmentedColormap.from_list("bwr", bwr)
-    #######################################################
 
     shap.image_plot(shap_values=shap_values, pixel_values=org_img.reshape(1, 224, 224, 3) / 255, cmap=bwr,
                     labels=labels, show=False)
@@ -30,4 +30,3 @@ def explain(model, img, org_img, labels):
     plt.close()
 
     return fig
-    # return shap_values
